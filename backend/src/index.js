20e32cd7517abf99b83b8ecf6817217616736853
Alison Gustavo de Lima <alisonlima2@hotmail.com>
2020-05-21 15:15:33 -0300

    Finalização backend

---
+++ backend/src/index.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+
+const routes = require("./routes");
+
+const app = express();
+
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.set("useCreateIndex", true);
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(process.env.PORT || 3333);
